4b8c30abc266b8c8be2b21bca1690593921f678e
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-26 21:15:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const colorSelect = document.querySelector("#colorSelect");
+const removeBtn = document.querySelector("input[type='button']");
+
+removeBtn.addEventListener(('click'), () => {
+	const options = colorSelect.selectedIndex;
+	if(options != -1){
+		colorSelect.remove(options);
+	}
+});
